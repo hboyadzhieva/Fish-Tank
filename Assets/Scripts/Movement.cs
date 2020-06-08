9200d5f3b11c183d71a29de5a55764413e8b02bf
Hristina Boyadzhieva <hristinaboyadzhieva@gmail.com>
2020-06-08 17:11:40 +0300

    Added movement script for player

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using static UnityEngine.Mathf;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    [Range(1, 5)]
+    private float speed = 3;
+
+    [SerializeField]
+    private TankBoundaries tankBoundaries;
+
+    private BoxCollider2D collider;
+
+    private float height;
+
+    private float currentScale;
+
+    private readonly float movementThreshold = 0.1f;
+    void Start()
+    {
+        collider = gameObject.GetComponent<BoxCollider2D>();
+        height = collider.bounds.size.y;
+        currentScale = transform.localScale.x;
+    }
+
+    void Update()
+    {
+        moveHorizontal();
+        moveVertical();
+        ResolveLookDirection();
+    }
+
+    private void moveHorizontal()
+    {
+        Vector3 horizontalMovement;
+        if (transform.position.x > tankBoundaries.RightBoundary())
+        {
+            horizontalMovement = new Vector3(tankBoundaries.LeftBoundary(), transform.position.y, 0f);
+            transform.position = horizontalMovement;
+        }
+        else if (transform.position.x < tankBoundaries.LeftBoundary()) {
+            horizontalMovement = new Vector3(tankBoundaries.RightBoundary(), transform.position.y, 0f);
+            transform.position = horizontalMovement;
+        }
+        else
+        {
+            horizontalMovement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+            transform.position += horizontalMovement * Time.deltaTime * speed;
+        }
+        
+    }
+
+    private void moveVertical()
+    {
+        Vector3 verticalMovement;
+        float maxVertical = tankBoundaries.TopBoundary() - height / 2;
+        float minVertical = tankBoundaries.BottomBoundary() + height / 2;
+        if (transform.position.y > maxVertical) {
+            verticalMovement = new Vector3(transform.position.x, maxVertical, 0f);
+            transform.position = verticalMovement;
+        }
+        else if (transform.position.y < minVertical)
+        {
+            verticalMovement = new Vector3(transform.position.x, minVertical, 0f);
+            transform.position = verticalMovement;
+        }
+        else
+        {
+            verticalMovement = new Vector3(0f, Input.GetAxis("Vertical"), 0f);
+            transform.position += verticalMovement * Time.deltaTime * speed;
+        }
+    }
+
+    private void ResolveLookDirection()
+    {
+        if (Abs(Input.GetAxis("Horizontal")) > movementThreshold) {
+            Debug.Log(Input.GetAxis("Horizontal"));
+            transform.localScale = new Vector3(Sign(Input.GetAxis("Horizontal"))*currentScale, currentScale, 1f);
+        }
+    }
+}
---
+++ Assets/Scripts/TankBoundaries.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankBoundaries : MonoBehaviour
+{
+
+    private Vector2 screenMaxBounds;
+    private Vector2 screenMinBounds;
+    void Start()
+    {
+        screenMaxBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        screenMinBounds = -screenMaxBounds;
+    }
+
+    public float TopBoundary() {
+        return screenMaxBounds.y;
+    }
+
+    public float RightBoundary() {
+        return screenMaxBounds.x;
+    }
+
+    public float BottomBoundary()
+    {
+        return screenMinBounds.y;
+    }
+
+    public float LeftBoundary()
+    {
+        return screenMinBounds.x;
+    }
+
+}
