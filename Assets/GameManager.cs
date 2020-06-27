e743a2665390cd23db545cb456a0c419f03749d2
Hristina Boyadzhieva <hristinaboyadzhieva@gmail.com>
2020-06-27 20:10:56 +0300

    added game over menu

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,17 +7,25 @@ public class GameManager : MonoBehaviour
 {
     bool gameHasEnded = false;
     public float restartDelay = 1f;
+    [SerializeField]
+    private GameObject gameOverMenu;
+
     public void gameOver()
     {
         if (gameHasEnded == false)
         {
             gameHasEnded = true;
-            Debug.Log("GAME OVER");
+            gameOverMenu.SetActive(true);
         }
     }
 
     public void restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void loadMainMenu()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }
